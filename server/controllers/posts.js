81bb8af3130df39ea1707fff4aab385c7c1ee609
dilipksahu <sahud048@gmail.com>
2021-09-22 22:06:53 +0530

    Redux store created and api called in action

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -3,6 +3,7 @@ import PostMessage from "../models/postMessage.js";
 export const getPosts = async (req, res) => {
     try {
         const postMessage = await PostMessage.find();
+        console.log("getPosts called")
         res.status(200).json(postMessage);
     } catch (error) {
         res.status(404).json({ message: error.message });
