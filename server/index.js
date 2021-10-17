cfd572eb3d27dc77d6d0d63b019e974c29a680a3
dilipksahu <sahud048@gmail.com>
2021-10-17 12:01:35 +0530

    action types constants maintain and also in backend .env maint

--- server/index.js
+++ server/index.js
@@ -2,10 +2,12 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -15,11 +17,11 @@ app.use('/posts', postRoutes);
 
 //https://cloud.mongodb.com/v2/6130f63d0be94e751fa53655#security/network/accessList
 // const CONNECTION_URL = 'mongodb+srv://mernstackproject:mernstackproject123@cluster0.z51bk.mongodb.net/mernstackproject?retryWrites=true&w=majority';
-const CONNECTION_URL = "mongodb://localhost:27017/mernstackproject";
+// const CONNECTION_URL = "mongodb://localhost:27017/mernstackproject";
 // console.log("CONNECTION_URL===>", CONNECTION_URL);
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
     .catch((error) => console.log(`${error} did not connect`));
 
